0228e99fcef60f4db8738308c9b2928222237fc2
luki <bulukless@gmail.com>
2022-10-11 09:15:47 +0700

    Update app.js

--- SQL+Tableau/app.js
+++ SQL+Tableau/app.js
@@ -3,7 +3,7 @@ console.log('Is this working?');
 let viz;
 
 //Add Share Link to Tableau Public in here
-const url = "https://public.tableau.com/views/Covid19-Portofolio1/Dashboard1?:language=en-US&:display_count=n&:origin=viz_share_link";
+const url = "https://prod-apsoutheast-a.online.tableau.com/t/lukiprasetyo/views/psql-learn-heroku-store/Dashboard1/6c29c2dd-0bbf-4d75-aedf-c903b3900cbc/728115e7-3998-482e-9f32-e3e44be75661?:display_count=n&:showVizHome=n&:origin=viz_share_link";
 
 const vizContainer = document.getElementById('vizContainer');
 const options = {
